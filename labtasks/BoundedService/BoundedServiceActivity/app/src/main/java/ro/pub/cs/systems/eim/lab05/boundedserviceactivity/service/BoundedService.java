b9422af298c9ddff4583e85fd9290f8dfab87a8f
Mihai Vlad C <mihai.vlad.catalina@gmail.com>
2020-04-12 22:47:21 +0300

    implemented the rest of the exercises

--- labtasks/BoundedService/BoundedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/boundedserviceactivity/service/BoundedService.java
+++ labtasks/BoundedService/BoundedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/boundedserviceactivity/service/BoundedService.java
@@ -2,6 +2,7 @@ package ro.pub.cs.systems.eim.lab05.boundedserviceactivity.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -11,14 +12,33 @@ import ro.pub.cs.systems.eim.lab05.boundedserviceactivity.general.Constants;
 
 public class BoundedService extends Service {
 
-    final private IBinder boundedServiceBinder = null;
+    final private IBinder boundedServiceBinder = new BoundedServiceBinder();
     final private Random random = new Random();
 
-    // TODO: exercise 10a - implement a IBinder public class to provide a reference
+    // exercise 10a - implement a IBinder public class to provide a reference
     // to the service instance through a getService() public method
 
-    // TODO: exercise 10f - override the lifecycle callback method and log a message
+    public class BoundedServiceBinder extends Binder {
+        public BoundedService getService() {
+            return BoundedService.this;
+        }
+    }
+
+    // : exercise 10f - override the lifecycle callback method and log a message
     // of the moment they are invoked
+    @Override
+    public void onCreate() { Log.d(Constants.TAG, "onCreate()"); }
+    @Override
+    public void onDestroy() { Log.d(Constants.TAG, "onDestroy()"); }
+    @Override
+    public boolean onUnbind(Intent intent) { Log.d(Constants.TAG, "onUnbind()"); return true; }
+    @Override
+    public void onRebind(Intent intent) { Log.d(Constants.TAG, "onRebind()"); }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(Constants.TAG, "OnStartCommand()");
+        return START_REDELIVER_INTENT; // schedule for restart, on crash
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -27,8 +47,9 @@ public class BoundedService extends Service {
     }
 
     public String getMessage() {
-        // TODO: exercise 10b - return a random value from the Constants.MESSAGES array list
-        return null;
+        //  exercise 10b - return a random value from the Constants.MESSAGES array list
+        Log.d(Constants.TAG, "getMessage()");
+        return Constants.MESSAGES.get(random.nextInt(Constants.MESSAGES.size()));
     }
 
 }
